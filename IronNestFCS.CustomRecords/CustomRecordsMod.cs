5cd4a5415fc7803240e3c0cf261ed59e48b48b4a
svr2kos2 <svr2kos2@hotmail.com>
2026-08-07 07:17:28 +0800

    chore: bump up CustomRecords version

--- IronNestFCS.CustomRecords/CustomRecordsMod.cs
+++ IronNestFCS.CustomRecords/CustomRecordsMod.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using Object = UnityEngine.Object;
 
 [assembly: MelonInfo(typeof(IronNestFCS.CustomRecords.CustomRecordsMod), 
-    "IronNestFCS.CustomRecords", "1.0.0", "svr2kos2")]
+    "IronNestFCS.CustomRecords", "1.0.1", "svr2kos2")]
 [assembly: MelonGame("Iron Nest", "Iron Nest: Heavy Turret Simulator")]
 
 namespace IronNestFCS.CustomRecords;
